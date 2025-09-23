d7f8520490cb7a9a406bfd672663808265104ea8
augmil <augustmilliken15@gmail.com>
2025-09-23 17:41:16 -0600

    cleaned up slightly

--- dataPull.py
+++ dataPull.py
@@ -1,23 +1,7 @@
 from pybaseball import statcast
 import pandas as pd
-from datetime import datetime
-import seaborn as sns
-from matplotlib import pyplot as plt
 from multipledispatch import dispatch
 from tqdm import tqdm
-# import regex as rx
-
-# data = statcast(start_dt="2025-09-14", end_dt="2025-09-14")
-# date_format = "%Y-%m-%d %H:%M:%S"
-# pd.set_option('display.max_columns', None)
-# for col in data.select_dtypes(exclude=['object', 'double', 'bool', 'datetime']).columns:
-#     print(f'{col}: {list(data.groupby(col).groups.keys())}\n')
-# print(data['description'])
-# x_group = data.groupby('game_pk')
-# print(x_group.get_group(list(x_group.groups.keys())[0]))
-# df = data.drop(columns=data.select_dtypes(include='object').columns)
-# sns.heatmap(df.corr())
-# plt.show()
 
 class dataProcesser():
     def __init__(self, start: str, end: str):
@@ -37,7 +21,6 @@ class dataProcesser():
         NOTE: This function uses the multiple dispatch package so the pop up shown may not represent the correct function. One 
         uses a pre-existing key and the other does not.
         '''
-        #NOTE: prob easier to have data as global so that we can chnage multiple 
         print(f"Converting {' '.join(col_name.split(sep='_'))} with no key...")
         var = 0
         key = {}
@@ -54,7 +37,7 @@ class dataProcesser():
             self.keys[col_name] = key
             return True
         except Exception as e:
-            # print(e)
+            print(e)
             return False
         
     @dispatch(str, bool) #NOTE: does self effect this?
@@ -75,7 +58,7 @@ class dataProcesser():
                 self.data.iloc[i, col_name] = self.data.keys[value]
             return True
         except Exception as e:
-            # print(e)
+            print(e)
             return False
 
     def clean_data(self) -> bool:
@@ -85,7 +68,6 @@ class dataProcesser():
         Returns bool representing if the data is usebale with the data stored in the object and, if true, also assigned to a CSV file 
         for storage.
         '''
-        # fix so we get columns 
         # NOTE: we need to have a case to handle "des" given it is never specific
         skip = []
         try:
@@ -93,7 +75,7 @@ class dataProcesser():
                 if pd.NA in self.data[col]:
                     self.empty[col] = self.data[col] #check
                     skip.append(col)
-            for col in self.data.select_dtypes(include="object").columns: # NOTE: should pull strings maybe try str?
+            for col in self.data.select_dtypes(include="object").columns:
                 if col not in self.keys.keys():
                     if not self.convert_to_ordinal(col):
                         return False
@@ -130,41 +112,3 @@ class dataProcesser():
 
         
     
-
-
-
-
-
-
-
-
-
-'''
-there is no umpire data it appears
-
-need to figure out what the original index is and maybe not use 
-
-what differentiates between events and description 
-des is like commentary 
-use des to create embeddings from data that could be similar to the description and then predit
-using those (re-pass the game state embedding to ensure that the info is still prevelent) ie created embedding would represent the 
-outcome in text and the game state embedding would be the before 
-
-events, description, game_type, stand, p_throws, home_team, away_team, type, bb_type, inning_topbot, and the below are all strings 
-
-player_name, des, pitch_name, if_fielding, and of_fielding might be the most difficult to convert to numeric
-
-need to remove the empty values 
-
-where do we want the cut off for a given swing? 
-only use values that are prior to the hit so pitch info is okay but nothing beyond that 
-
-what do we want to predict? 
-
-we could do weights for players or weights for years or weights for game types, weights 
-for position etc
-
-need to ensure unique swing for each instance 
-make negative examples for the game state to outcome embedding 
-description is short hand for the outcome
-'''
