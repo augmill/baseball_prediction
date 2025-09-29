a7a80a53aa9095f3ede6badeb2278d682c36302a
Jackson Cockrum <jaco7466@colorado.edu>
2025-09-29 16:22:42 -0600

    small updates to GetData.py

--- database/scripts/GetData.py
+++ database/scripts/GetData.py
@@ -1,19 +1,24 @@
+"""
+Used by GetData.sh to pull raw statcast data from pybaseball
+"""
 from pybaseball import statcast
-import pandas as pd
-from multipledispatch import dispatch
 import sys
 
-def main(start_date, end_date, file_path):
-    # Fetch data from statcast
-    data = statcast(start_dt=start_date, end_dt=end_date)
-    # Save to CSV
-    data.to_csv(file_path, index=False)
+if len(sys.argv) != 4:
+    print("Usage: python3 GetData.py <start_date> <end_date> <file_path>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python GetData.py <start_date> <end_date> <file_path>")
-        sys.exit(1)
-    start_date = sys.argv[1]
-    end_date = sys.argv[2]
-    file_path = sys.argv[3]
-    main(start_date, end_date, file_path)
+start_date = sys.argv[1]
+end_date = sys.argv[2]
+file_path = sys.argv[3]
+
+try:
+    statcast(start_dt=start_date, end_dt=end_date).to_csv(file_path)
+except Exception as e:
+    print(e)
+    print(f"Error: failed to pull data for {start_date} to {end_date}")
+    sys.exit(1)
+
+
+
+
