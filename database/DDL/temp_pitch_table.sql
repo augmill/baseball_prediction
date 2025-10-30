2d6d77544de3f80c5531046c3ced5ae0d9e4cc05
JRCockrum <jaco7466@colorado.edu>
2025-10-30 12:52:29 -0600

    Finished DDL for atbat_facts table

--- database/DDL/temp_pitch_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS baseball-prediction-473623.test_dataset.test_pitches ( 
-    game_date DATE, 
-    pitch_type STRING, 
-    release_speed FLOAT64, 
-    release_pos_x FLOAT64, 
-    release_pos_z FLOAT64 
-);
